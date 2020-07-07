1303071d82549b1628056f97b425bc1dc3d8d830
Arvmor <Arvin.morawej@gmail.com>
2020-07-07 15:12:20 +0430

    Add two new function

--- main.py
+++ main.py
@@ -10,31 +10,40 @@ from credentials import account
 
 def signal_handler(signal, frame):  # Handle Ctrl-C
     # system("sudo service apache2 stop")
+    print("Closing the script")
     driver.close()
     exit(0)
 
 
-def login(accountNumber):  # Login function
-    driver.find_element(
-        By.XPATH, '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input').send_keys(account[accountNumber][0])
+def login(numberOfAccount):  # Login function
     sleep(2)
     driver.find_element(
-        By.XPATH, '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input').send_keys(account[accountNumber][1])
+        By.XPATH, '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input').send_keys(account[numberOfAccount][0])
     sleep(2)
     driver.find_element(
-        By.XPATH, '//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]/button'
+        By.XPATH, '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input').send_keys(account[numberOfAccount][1])
+    sleep(2)
+    driver.find_element(
+        By.XPATH, '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button'
     ).click()
     sleep(5)
-    # Close the annoying alerts
-    driver.refresh()
-    if driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/section/div/div[2]').text == 'Save Your Login Info?':
-        driver.find_element(
-            By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/div/button'
-        ).click()
-    if driver.find_element(By.XPATH, '/html/body/div[4]/div/div/div/div[2]/h2').text == 'Turn on Notifications':
-        driver.find_element(
-            By.XPATH, '/html/body/div[4]/div/div/div/div[3]/button[2]'
-        ).click()
+
+
+def sendComment(commentText, postURL):  # Send Comments
+    driver.get(postURL)
+    sleep(3)
+    driver.find_element(
+        By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[2]/section[3]/div/form/textarea').send_keys(commentText)
+    sleep(10)
+    driver.find_element(
+        By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[2]/section[3]/div/form/button').click()
+
+
+def sendLike(postURL):  # Like the post
+    driver.get(postURL)
+    sleep(3)
+    driver.find_element(
+        By.XPATH, '//*[@id="react-root"]/section/main/div/div[1]/article/div[2]/section[1]/span[1]/button').click()
 
 
 # Driver settings
@@ -44,15 +53,15 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--log-level=3")
-chrome_options.page_load_strategy = 'eager'
+chrome_options.add_argument(
+    "user-data-dir=/home/r00t/.config/chromium/")
 chrome_options.add_argument("--log-level=OFF")
 driver = webdriver.Chrome("chromedriver", options=chrome_options)
-driver.get("https://www.instagram.com/accounts/onetap/")
+driver.get("https://www.instagram.com")
+
 # Main code
-try:
-    login(0)
-    sleep(5)
-    driver.close()
-except:
-    print("Crashed !")
-    driver.close()
+login(1)
+sendComment(
+    "Post Zibayi bood !", 'https://www.instagram.com/p/CBELcX9F361/')
+sendLike('https://www.instagram.com/p/CBELcX9F361/')
+driver.close()
